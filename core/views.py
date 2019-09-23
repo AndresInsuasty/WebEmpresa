2aa15c1f6991145f864a76b3af6034100920b10f
Andres <andresinsuastyd10@gmail.com>
2019-09-23 14:57:11 -0500

    primera practica, conf vistas

--- core/views.py
+++ core/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse('Inicio home/')
+
+def about(request):
+    return HttpResponse('Historia about/')
+
+def services(request):
+    return HttpResponse('Servicios services/')
+
+def store(request):
+    return HttpResponse('Visítanos store/')
+
+def contact(request):
+    return HttpResponse('Contacto contact/')
+
+def blog(request):
+    return HttpResponse('Blog blog/')
+
+def sample(request):
+    return HttpResponse('Sample sample/')

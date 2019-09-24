1cb96af1846e247f29e38275b099992ce90d0990
Andres <andresinsuastyd10@gmail.com>
2019-09-23 22:45:23 -0500

    creando app de servicios

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     #local apps
     'core',
+    'services.apps.ServicesConfig',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
@@ -121,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media Files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
